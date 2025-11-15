9e17a78b523660be9e03f516e2cdd41bfb64d1ab
Kachalla Ahmad Galadima <ahmadkgaladima@gmail.com>
2025-11-15 23:13:32 +0100

    Added a weather_advice code file

--- python_introduction/future_age_calculator.py
+++
@@ -1,8 +0,0 @@
-# A python script to calculate the user's age in a future year
-
-current_age = eval(input("How old are you? "))
-current_year = 2050-2023
-future_age = current_year + current_age
-
-print(f"In 2050, you will be {future_age} years old.")
-
--- python_introduction/simple_interest.py
+++
@@ -1,9 +0,0 @@
-# A Python script that calculates the simple interest earned on an investment over a period of time
-
-principal = 1000
-rate = 0.05
-time = 3
-
-interest = principal * rate * time 
-
-print(f"The simple interest is: {interest}")
