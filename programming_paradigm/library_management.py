2ded4e71e94a1cc260fd98c900786f8ab8bb5f87
Kachalla Ahmad Galadima <ahmadkgaladima@gmail.com>
2025-11-30 21:39:52 +0100

    Add library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,53 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out if available."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as available again."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # private list of Book objects
+
+    def add_book(self, book):
+        """Add a new book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by its title."""
+        for book in self._books:
+            if book.title == title:
+                return book.check_out()
+        return False  # book not found
+
+    def return_book(self, title):
+        """Return a book by its title."""
+        for book in self._books:
+            if book.title == title:
+                return book.return_book()
+        return False  # book not found
+
+    def list_available_books(self):
+        """Print all books that are not checked out."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
+
