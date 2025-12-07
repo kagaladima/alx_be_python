b65b5c51b7924b673d96be95e39134b68d13b9f7
Kachalla Ahmad Galadima <ahmadkgaladima@gmail.com>
2025-12-07 22:05:23 +0100

    Add book_class.py file

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,25 +1,17 @@
-
-# book_class.py
-
 class Book:
     def __init__(self, title, author, year):
-        """Initialize book attributes."""
         self.title = title
         self.author = author
         self.year = year
 
-    def __del__(self):
-        """Destructor message."""
-        print(f"Deleting {self.title}")
-
     def __str__(self):
-        """String representation."""
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        """Official representation."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
 
 class Library:
@@ -49,5 +41,7 @@ class Library:
         for book in self._books:
             if book.is_available():
                 print(f"{book.title} by {book.author}")
+
+
 
 
