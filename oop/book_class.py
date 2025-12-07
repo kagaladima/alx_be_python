ad9c44dfeb67fd42b8e744285d83bebabce819c1
Kachalla Ahmad Galadima <ahmadkgaladima@gmail.com>
2025-12-07 19:31:17 +0100

    Add book_class.py version 2 file

--- oop/book_class.py
+++ oop/book_class.py
@@ -51,3 +51,21 @@ class Library:
             if book.is_available():
                 print(f"{book.title} by {book.author}")
 
+
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
